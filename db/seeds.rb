8787c3ded93da3990bea481c2b64f36a905b8755
james williams & alex castaneda <student@epicodus.com>
2015-07-22 14:30:10 -0700

    being fixing ember models to mimic ruby

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 10.times do
 
-  Artist.create(name:'john denver', description: 'country weedrock', money: '$250')
+  Song.create(name: 'Butt Rock', genre: 'Electro House', key: '7A', bpm: '130', description: 'Big drop', price: '5')
 end
 
 if !User.any?
-  User.create(email: 'test@test.com', password: 'password')
+  User.create(email: 'artist@test.com', password: 'password', artist_name: 'Deadmau5p00l', artist: true, name: 'Bob Denver', money: '125', description: 'maouse head mofo')
+  User.create(email: 'user@test.com', password: 'password', artist: false, name: 'Fan Blow', money: '500', description: 'I am a fan')
 end
