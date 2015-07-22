f8bd34a82082ed9f1d4a37371f97e90389099031
steve 4 & jordan <student@epicodus.com>
2015-07-22 13:28:05 -0700

    add back end user auth with devise.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722163757) do
+ActiveRecord::Schema.define(version: 20150722174946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20150722163757) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "artists_songs", id: false, force: :cascade do |t|
+    t.integer "artist_id"
+    t.integer "song_id"
+  end
+
+  add_index "artists_songs", ["artist_id"], name: "index_artists_songs_on_artist_id", using: :btree
+  add_index "artists_songs", ["song_id"], name: "index_artists_songs_on_song_id", using: :btree
+
   create_table "songs", force: :cascade do |t|
     t.string   "name"
     t.string   "genre"
@@ -35,12 +43,30 @@ ActiveRecord::Schema.define(version: 20150722163757) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "songs_users", id: false, force: :cascade do |t|
+    t.integer "song_id"
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "password"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "email",                  default: "", null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "authentication_token",   default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
